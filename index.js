49b7de22f31f42143ee3605d91ccec206e4f125c
Maryam6713 <maryammalik6713@gmail.com>
2024-05-28 15:12:04 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+S//---------------Array-Methods-------------------//
+//---------------Pop-----------------------//
+
+var numbers = [8 , 16 , 24 , 32 , 40 , 48 , 56 , 64 , 72 , 80]
+//for define pop
+numbers.pop()
+numbers.pop()
+numbers.pop()
+console.log(numbers)
+
+
+//for return
+ var returnValue = numbers.pop()
+console.log(returnValue)
+
+
+//---------------Push------------//
+
+var numbers = [8 , 16 , 24 , 32 , 40 , 48 , 56 , 64 , 72 , 80]
+//for define push
+numbers.push(88 , 96 )
+console.log(numbers)
+//for return
+ var returnValue = numbers.push()
+console.log(returnValue)
+
+//--------------Shift------------//
+
+var numbers = [8 , 16 , 24 , 32 , 40 , 48 , 56 , 64 , 72 , 80]
+//for define shift
+numbers.shift( )
+numbers.shift( )
+console.log(numbers)
+//for return
+ var returnValue = numbers.shift()
+console.log(returnValue)
+
+//------------Unshift---------------//
+
+var numbers = [8 , 16 , 24 , 32 , 40 , 48 , 56 , 64 , 72 , 80]
+//for define unshift
+numbers.unshift( )
+console.log(numbers)
+//for return
+ var returnValue = numbers.unshift()
+console.log(returnValue)
+
+//-------------Splice------------//
+
+var numbers = [8 , 16 , 24 , 32 , 40 , 48 , 56 , 64 , 72 , 80]
+//for define splice
+numbers.splice(0,5,12, 24 )
+
+console.log(numbers)
+//for return
+ var returnValue = numbers.splice()
+console.log(returnValue)
+
+
+
+
+
+
+
+
